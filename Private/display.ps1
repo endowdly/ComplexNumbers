896155b76eedfef4e9e4a2cdea2ccc445c3a0eda
endowdly <endowdly@users.noreply.github.com>
2017-05-22 23:47:44 -0400

    Update display.ps1
    removed unnecessary lines

--- Private/display.ps1
+++ Private/display.ps1
@@ -1,7 +1,3 @@
-﻿# Require -Version 2.0
-
-# This is just a quick script with some things for fun display.
-
 # Utility Chars
 [char] $lf = 10
 [char] $cr = 13
@@ -9,13 +5,9 @@
 
 # Fun Chars
 if ($BasicSymbols) { 
-    [char] $ch = 0x221a     # a square root
-    [char] $ex = 0x00d7     # a light multiplication
     [char] $angle = 0x221F  # a right angle
 } 
 else { 
-    [char] $ch = 0x2714     # heavy check
-    [char] $ex = 0x2716     # heavy multiply
     [char] $angle = 0x2220  # an acute angle
 }
 
